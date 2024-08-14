49cab3c13e4680e03db60ae8fd6c448a760a10f2
Denis Rykov <d.rykov@ttc.kz>
2024-08-14 23:36:19 +0500

    fix

---
+++ lib/constants/colors/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const transparent = Color(0x00000000);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color text = Color(0xFF343A40);
+  static const Color buttonBlue = Color(0xFF007BFF);
+  static const Color buttonGreen = Color(0xFF28A745);
+  static const Color buttonRed = Color(0xFFDC3545);
+}
